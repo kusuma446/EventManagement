dc1436ecf773b149dc2bf12aa55aa212f9373749
Arif_Kurniawan <kangarif.mas@gmail.com>
2025-04-30 21:11:40 +0700

    fix: improve token/jwt login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -99,9 +99,34 @@ export const loginService = async (body: LoginRequestBody) => {
     throw { status: 401, message: "Invalid credential" };
   }
 
-  const token = jwt.sign({ id: user.id, role: user.role }, secret_token, {
-    expiresIn: "1d",
-  });
+  // // dispatch(
+  //   onLogin({
+  //     user: {
+  //       email: data.data.user.email,
+  //       first_name: data.data.user.first_name,
+  //       last_name: data.data.user.last_name,
+  //       role: data.data.user.role,
+  //       avatar: data.data.user.avatar, // jika tersedia
+  //     },
+  //     isLogin: true,
+  //   })
+  // );
+
+  // *singkron dengan redux
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      role: user.role,
+      profile_pict: user.profile_pict,
+    },
+    secret_token,
+    {
+      expiresIn: "1d",
+    }
+  );
   return { user, token };
 };
 
