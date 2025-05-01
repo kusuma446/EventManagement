6e4d907ccdc7360ed0f8209d83e9fd5d1be66ef7
kusuma446 <yukikusuma65@gmail.com>
2025-05-01 19:33:21 +0800

    adding get ticket type

--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import { createTicketTypeService } from "../services/ticket.service";
+import {
+  createTicketTypeService,
+  getTicketTypesService,
+} from "../services/ticket.service";
 
 export const createTicketType = async (
   req: Request,
@@ -11,5 +14,18 @@ export const createTicketType = async (
     res.status(201).json({ message: "Ticket type created", data });
   } catch (error) {
     next(error);
+  }
+};
+
+export const getTicketTypes = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const data = await getTicketTypesService(req);
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
   }
 };
